a3d13ad6786f6bbe68691f5d1b4d3a6e1c0da773
Juyoung Jin <juyoung0520@naver.com>
2021-01-25 01:11:10 +0900

    sorting/boj_2751.java

---
+++ sorting/boj/boj_2751.java
@@ -0,0 +1,26 @@
+package baek_joon;
+import java.util.*;
+import java.io.*;
+
+public class sorting_2751 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		
+		for(int i = 0; i < n; i++) {
+			array.add(Integer.parseInt(br.readLine()));
+		}
+		
+		Collections.sort(array);
+		
+		for(int i : array) {
+			sb.append(i).append('\n');
+		}
+		System.out.println(sb);
+	}
+
+}
