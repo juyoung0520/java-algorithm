fa425900362ed72fccacfe84b3420affefbcbd81
Juyoung Jin <juyoung0520@naver.com>
2021-01-27 18:54:49 +0900

    dfs&bfs/boj/p1260.py

---
+++ dfs&bfs/boj/p1260.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v,end=' ')
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = False
+
+    while queue:
+        v = queue.popleft()
+        print(v,end=' ')
+
+        for i in graph[v]:
+            if visited[i]:
+                queue.append(i)
+                visited[i] = False
+
+n, m, start = map(int,sys.stdin.readline().split())
+graph = [[] for  i in range(n+1)]
+visited = [False for i in range(n+1)]
+
+for i in range(m):
+        v1, v2 = map(int, sys.stdin.readline().split())
+        graph[v1].append(v2)
+        graph[v2].append(v1)
+
+for i  in range(1, n+1):
+    graph[i].sort()  #인접노드 작은 것부터 방문하기로 해서.. 행렬로 표현하면 정렬안해도 됨.
+
+dfs(graph, start, visited)
+print()
+bfs(graph, start, visited)
