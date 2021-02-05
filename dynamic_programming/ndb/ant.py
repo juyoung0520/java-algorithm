f9fa71ac4d4a1bbc411a82e641385cbe121e76a5
juyoung <juyoung0520@naver.com>
2021-02-05 14:37:45 +0900

    modified

--- dynamic_programming/ndb/ant.py
+++ dynamic_programming/ndb/ant.py
@@ -2,15 +2,14 @@ from sys import stdin
 
 n = int(stdin.readline())
 foods = list(map(int, stdin.readline().split()))
+# 인덱스 창고까지의 최대값
 d = [0] * 100
 
-#다이나믹 프로그래밍 (보텀업)
-d[0] = array[0]
-d[1] = max(array[0], array[1])
+# 다이나믹 프로그래밍 (보텀업)
+d[0] = foods[0]
+d[1] = max(foods[0], foods[1])
 for i in range(2, n):
+    # i번째 창고를 선택하지 않을 경우와 선택할 경우 중 최대값
     d[i] = max(d[i - 1], d[i - 2] + foods[i])
 
 print(d[n - 1])
-        
-
-
