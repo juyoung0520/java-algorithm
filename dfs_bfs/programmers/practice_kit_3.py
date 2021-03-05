d4bad9f8b3ebbcdddf65cb2b8ad8877adc302cd9
Juyoung Jin <juyoung0520@naver.com>
2021-03-05 15:09:42 +0900

    dfs_bfs/programmers/practice_kit_3.py add explain

--- dfs_bfs/programmers/practice_kit_3.py
+++ dfs_bfs/programmers/practice_kit_3.py
@@ -1,5 +1,6 @@
 import collections
 
+# 다른 글자 1개인 단어 모인 배열 반환
 def findDifferent(current, words):
     array = []
 
@@ -16,12 +17,13 @@ def findDifferent(current, words):
     return array
 
 def solution(begin, target, words):
+    # 타깃 words에 없으면 리턴 0
     if not target in words:
         return 0
-
+    # 각 단어의 거리를 값으로 갖는 딕셔너리
     dist = {begin: 0}
     que = collections.deque([begin])
-
+    # 각 단어까지의 거리를 구하면서 BFS
     while que:
         current = que.popleft()
 
@@ -29,5 +31,5 @@ def solution(begin, target, words):
             if word not in dist:
                 dist[word] = dist[current] + 1
                 que.append(word)
-        
+    # 타깃의 거리 반환   
     return dist.get(target)
