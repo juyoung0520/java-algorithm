dc6f3f85d32c813634344e78cc281a0d544000cb
juyoung0520 <juyoung0520@naver.com>
2022-06-24 14:38:32 +0900

    new: 백준 백트래킹 -15650 -15651 -15652

---
+++ backtracking/boj/P15651.java
@@ -0,0 +1,42 @@
+package backtracking.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P15651 {
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+
+        dfs(n, m, 0);
+
+        System.out.println(sb.toString());
+        br.close();
+    }
+
+    public static void dfs(int n, int m, int idx) {
+        if (idx == m) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[idx] = i + 1;
+            dfs(n, m, idx + 1);
+        }
+
+    }
+    
+}
---
+++ backtracking/boj/P15652.java
@@ -0,0 +1,40 @@
+package backtracking.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P15652 {
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+
+        dfs(n, m, 0, 0);
+
+        System.out.println(sb.toString());
+        br.close();
+    }
+
+    public static void dfs(int n, int m, int idx, int start) {
+        if (idx == m) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            arr[idx] = i + 1;
+            dfs(n, m, idx + 1, i);
+        }
+    }
+}
